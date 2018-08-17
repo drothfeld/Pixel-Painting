5b0ee5870e0fc2641d456bbd616c6ee4b2dc3459
Dylan Rothfeld <drothfel@gmail.com>
2018-08-17 15:14:50 -0700

    initial commit

---
+++ Pixel Painting/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Pixel Painting
+//
+//  Created by Dylan Rothfeld on 8/17/18.
+//  Copyright © 2018 Dylan Rothfeld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
+}
+
